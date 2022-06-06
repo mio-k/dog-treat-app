476293e577af1a824cdd093fd7de258a4c08f422
mio kataoka <mio.kanna@gmail.com>
2022-06-06 15:46:42 -0700

    added the About component

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About(){
+    return(
+        <div>
+        <h1>Dog Pod's Chewy Order App</h1>
+        <p>The Dog Pod is a group of dog owners in my neighborhood. We dog sit each other's dogs, and have fun dog-themed get-togeters often.</p>
+        <p>One of the things we often do also is to combine our Chewy orders to get free shipping. Chewy offers free shipping over $49 worth of purchases.</p>
+        <p>Typically we'll be slacking each other, asking if anyone wants to add their orders, but I decided to use this scenario for my phase 2 project.</p>
+        <p>You can add your order to any of the product featured on the page.  You can filter them by the main ingredient, or search specific products.  And if the product you want is not shown, you can add it by submitting a request.</p>
+        <p>Once the total order goes over $49, Mio will place an order with Chewy and will post on the Dog Pod general channel when the package arrives!</p>
+        </div>
+    )
+}
+export default About;
