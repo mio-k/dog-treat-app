23840ad8da882c6d91f697a9d9e39ea4aed0e05f
mio kataoka <mio.kanna@gmail.com>
2022-06-08 15:38:21 -0700

    cleared the db.json to remove all the test data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Header from "./Header";
 
 function Home(){
   return(
     <div className="landing">
       <img src="../images/landingpageImage.jpg" alt="Home image" />
-      <h1>Welcome To The Dog Pod Treats!</h1>
     </div>
   )
 }
