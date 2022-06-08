4488f39c8fdec2ff00e1b53a3e0c8eb29e41a60a
mio kataoka <mio.kanna@gmail.com>
2022-06-08 15:35:45 -0700

    replaced the landing page image

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import Header from "./Header";
 function Home(){
   return(
     <div className="landing">
-      <img src="../images/defaultImage.png" alt="Home image" />
+      <img src="../images/landingpageImage.jpg" alt="Home image" />
       <h1>Welcome To The Dog Pod Treats!</h1>
     </div>
   )
