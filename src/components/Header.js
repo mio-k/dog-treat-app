dac981450382cbbfbbf44e090ca6af3d6bfe0706
mio kataoka <mio.kanna@gmail.com>
2022-06-08 15:17:18 -0700

    updated css for the nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,13 @@ import { NavLink } from "react-router-dom";
 
 function Header(){
     return (
+      <>
         <div className="header">
-          <p>test test</p>
           <strong className="title">DogPod Treats</strong>
-          <NavLink to="treats">Order Treats</NavLink>
-          <NavLink to="about">About This App</NavLink>
+          <NavLink className="nav" to="treats">Order Treats</NavLink>
+          <NavLink className="nav" to="about">About This App</NavLink>
         </div>
+      </>
     )
 }
 export default Header;
